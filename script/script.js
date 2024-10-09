24d73e5f1591cc61e7c9ce36a764d5a0bed5e1fa
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2024-10-09 22:47:31 +0330

    fixing the conflict

--- script/script.js
+++ script/script.js
@@ -1,12 +1,10 @@
 const showBtn = document.querySelector(".header-section-search button")
 const inputBox = document.querySelector(".header-section-search input")
-const backgroundBox = document.querySelector(".header-section")
 const locBox = document.getElementById("loc")
 const tempBox = document.getElementById("temp")
 const WindBox = document.getElementById("Wind")
 const HumidityBox = document.getElementById("Humidity")
 const PreciptationBox = document.getElementById("Preciptation")
-const conditionBox= document.getElementById("condition")
 const apiKey = "76a82f54cc0d476b86171625240510"
 showBtn.addEventListener('click', function() {
     getWeather(inputBox.value);
@@ -40,30 +38,22 @@ function getWeather(city) {
             WindBox.innerHTML=`${windSpeed}`;
             HumidityBox.innerHTML=`${humidity}`;
             PreciptationBox.innerHTML=`${Preciptation}`;
-            conditionBox.innerHTML=`${condition}`;
-            
-
-
-            let getMod = `${condition}`;
+            let getMod = data.current.condition.text
             if (getMod === "Sunny" || "Clear") {
-                document.body.style.backgroundImage = "url(image/sun-img.jpg)"
+                document.body.style.backgroundImage = "url(../image/sun-img.jpg)"
                 document.body.style.backgroundSize = "cover"
-                document.body.style.backgroundRepeat = "no-repeat"
             }
             if (getMod.includes("rain")) {
-                document.body.style.backgroundImage = "url(image/rain-img.jpg)"
+                document.body.style.backgroundImage = "url(../image/rain-img.jpg)"
                 document.body.style.backgroundSize = "cover"
-                document.body.style.backgroundRepeat = "no-repeat"
             }
             if (getMod.includes("cloudy")) {
-                document.body.style.backgroundImage = "url(image/cloud-img.jpg)"
+                document.body.style.backgroundImage = "url(../image/cloud-img.jpg)"
                 document.body.style.backgroundSize = "cover"
-                document.body.style.backgroundRepeat = "no-repeat"
             }
             if (getMod.includes("snow")) {
-                document.body.style.backgroundImage = "url(image/snow-img.jpg)"
+                document.body.style.backgroundImage = "url(../image/snow-img.jpg)"
                 document.body.style.backgroundSize = "cover"
-                document.body.style.backgroundRepeat = "no-repeat"
             }
           })
           .catch(error => {
@@ -79,8 +69,6 @@ document.addEventListener("DOMContentLoaded", function() {
             document.body.style.backgroundColor = "#2c3335";
             locBox.style.color = "#ddd";
             tempBox.style.color = "#ddd";
-            conditionBox.style.color = "#ddd";
-            backgroundBox.style.backgroundColor= "#333333c5";
             document.querySelector('.header-section-search button[type="submit"]').style.backgroundColor = "#0f172a";
             document.querySelectorAll('.section-info-part').forEach(box => {
                 box.style.backgroundColor="#4f6570";
@@ -93,8 +81,6 @@ document.addEventListener("DOMContentLoaded", function() {
             document.body.style.backgroundColor = "#02b4eb";
             locBox.style.color = "#0f172a";
             tempBox.style.color = "#0f172a";
-            conditionBox.style.color = "#0f172a";
-            backgroundBox.style.backgroundColor= "#ffffff59";
             document.querySelector('.header-section-search button[type="submit"]').style.backgroundColor = "#02b4eb";
             document.querySelectorAll('.section-info-part').forEach(box => {
                 box.style.backgroundColor="rgba(30, 62, 104, 0.671)";
